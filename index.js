9b65b92290e672068d912124c9258d561924d6bb
Kushal Khattri <kushalk62@gmail.com>
2021-12-09 16:44:42 -0800

    Created a writetofile function and used it generatemarkdown to write the file

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your title for your project?'
+    },
+    {
+        type: 'input',
+        name: 'decription',
+        message: 'Give a decription about your project?'
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err){
+        if(err) throw err 
+        console.log('file written');
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function(data){
+    writeToFile('README.md', generateMarkdown(data)); 
+    console.log(data)    
+    })
+}
 
 // Function call to initialize app
 init();
